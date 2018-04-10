7667d813132d4564a4c52ebdc5bd15e531296dbd
quantpy <quantpy@gmail.com>
2018-04-10 13:49:06 +0800

    create pkg matpy

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from .tools import mat2py, py2mat
+from ._matpy import MatPy
+
---
+++ test.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+@version: 0.1
+@author:  quantpy
+@file:    test.py
+@time:    2018/4/10 13:38
+"""
+
+from matpy import MatPy
+
+
+def main():
+    matpy = MatPy()
+
+
+if __name__ == '__main__':
+    pass
